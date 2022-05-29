e68b0855581032cf87e0b3f0f604e24ed38af913
RidwanaTripti <ridwanaakter17@gmail.com>
2022-05-30 03:23:50 +0600

    practice

---
+++ form_validation_hw/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Flutter\\SDK" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Flutter\\Code\\form_validation_hw" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Flutter\\SDK"
+  "PROJECT_DIR=D:\\Flutter\\Code\\form_validation_hw"
+  "FLUTTER_ROOT=D:\\Flutter\\SDK"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Flutter\\Code\\form_validation_hw\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Flutter\\Code\\form_validation_hw"
+  "FLUTTER_TARGET=D:\\Flutter\\Code\\form_validation_hw\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Flutter\\Code\\form_validation_hw\\.dart_tool\\package_config.json"
+)
